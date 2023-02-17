be486756e2f997e654084b76ca0c976d3a042aa9
asi-isa <ali.inceoglu@protonmail.com>
2023-02-17 22:29:50 +0100

    generate password

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,39 @@
     windows_subsystem = "windows"
 )]
 
+use serde::Deserialize;
+
+use passwords::PasswordGenerator;
+
+#[derive(Deserialize)]
+struct PasswordOption {
+    length: u8,
+    uppercase: bool,
+    lowercase: bool,
+    numbers: bool,
+    symbols: bool,
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn generate_password(option: PasswordOption) -> String {
+    let pg = PasswordGenerator {
+        length: option.length as usize,
+        numbers: option.numbers,
+        lowercase_letters: option.lowercase,
+        uppercase_letters: option.uppercase,
+        symbols: option.symbols,
+        spaces: false,
+        exclude_similar_characters: false,
+        strict: true,
+    };
+
+    pg.generate_one().unwrap()
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![generate_password])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,42 @@
+import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect, useState } from "react";
 import { AiOutlineCopy } from "react-icons/ai";
 
 function App() {
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    generateAndSetPassword();
+  }, []);
+
+  async function generatePassword(): Promise<string> {
+    return await invoke("generate_password", {
+      option: {
+        length: 21,
+        uppercase: true,
+        lowercase: true,
+        numbers: false,
+        symbols: true,
+      },
+    });
+  }
+
+  async function generateAndSetPassword() {
+    const password = await generatePassword();
+
+    setPassword(password);
+  }
+
+  async function onRegenerate() {
+    generateAndSetPassword();
+  }
+
   return (
     <div className="flex flex-col gap-4 w-72">
       <p className="text-center">Password Generator</p>
 
       <div className="flex justify-between items-center bg-[var(--bg-accent)] p-3">
-        <p>Hj?kU879§4$</p>
+        <p>{password}</p>
         <AiOutlineCopy className="text-[var(--accent)]" />
       </div>
 
@@ -51,7 +81,10 @@ function App() {
           </div>
         </div>
 
-        <div className="flex justify-center items-center bg-[var(--accent)] p-2">
+        <div
+          className="flex justify-center items-center bg-[var(--accent)] p-2 cursor-pointer"
+          onClick={onRegenerate}
+        >
           <p className="text-[var(--bg)]">REGENERATE</p>
         </div>
       </div>
